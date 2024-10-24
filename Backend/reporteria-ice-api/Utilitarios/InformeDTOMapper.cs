9a5602449bd3b5258e2689ff16b52e94dcecdff1
Alvaro <alvarosiles499@gmail.com>
2024-10-23 19:20:52 -0600

    Reutilizacion de los Mapper de los datos de Informe para InformeDTOMapper

--- Backend/reporteria-ice-api/Utilitarios/InformeDTOMapper.cs
+++ Backend/reporteria-ice-api/Utilitarios/InformeDTOMapper.cs
@@ -14,11 +14,17 @@ namespace reporteria_ice_api.Utilitarios
                 SubestacionId = informeDTO.SubestacionId,
                 LineaTransmisionId = informeDTO.LineaTransmisionId,
                 DatosDeLineaId = informeDTO.DatosDeLineaId,
+                DatosDeLinea = DatosDeLineaDTOMapper.ConvertirDTOADatosDeLinea(informeDTO.DatosDeLinea),
                 DatosGeneralesId = informeDTO.DatosGeneralesId,
+                DatosGenerales = DatosGeneralesDTOMapper.ConvertirDTOADatosGenerales(informeDTO.DatosGenerales),
                 TeleproteccionId = informeDTO.TeleproteccionId,
+                Teleproteccion = TeleproteccionDTOMapper.ConvertirDTOATeleproteccion(informeDTO.Teleproteccion),
                 DistanciaDeFallaId = informeDTO.DistanciaDeFallaId,
+                DistanciaDeFalla = DistanciaDeFallaDTOMapper.ConvertirDTOADistanciaDeFalla(informeDTO.DistanciaDeFalla),
                 TiemposDeDisparoId = informeDTO.TiemposDeDisparoId,
+                TiemposDeDisparo = TiemposDeDisparoDTOMapper.ConvertirDTOATiemposDeDisparo(informeDTO.TiemposDeDisparo),
                 CorrientesDeFallaId = informeDTO.CorrientesDeFallaId,
+                CorrientesDeFalla = CorrientesDeFallaDTOMapper.ConvertirDTOACorrientesDeFalla(informeDTO.CorrientesDeFalla),
                 Estado = informeDTO.Estado
             };
         }
@@ -32,11 +38,17 @@ namespace reporteria_ice_api.Utilitarios
                 SubestacionId = informe.SubestacionId,
                 LineaTransmisionId = informe.LineaTransmisionId,
                 DatosDeLineaId = informe.DatosDeLineaId,
+                DatosDeLinea = DatosDeLineaDTOMapper.ConvertirDatosDeLineaADTO(informe.DatosDeLinea),
                 DatosGeneralesId = informe.DatosGeneralesId,
+                DatosGenerales = DatosGeneralesDTOMapper.ConvertirDatosGeneralesADTO(informe.DatosGenerales),
                 TeleproteccionId = informe.TeleproteccionId,
+                Teleproteccion = TeleproteccionDTOMapper.ConvertirTeleproteccionADTO(informe.Teleproteccion),
                 DistanciaDeFallaId = informe.DistanciaDeFallaId,
+                DistanciaDeFalla = DistanciaDeFallaDTOMapper.ConvertirDistanciaDeFallaADTO(informe.DistanciaDeFalla),
                 TiemposDeDisparoId = informe.TiemposDeDisparoId,
+                TiemposDeDisparo = TiemposDeDisparoDTOMapper.ConvertirTiemposDeDisparoADTO(informe.TiemposDeDisparo),
                 CorrientesDeFallaId = informe.CorrientesDeFallaId,
+                CorrientesDeFalla = CorrientesDeFallaDTOMapper.ConvertirCorrientesDeFallaADTO(informe.CorrientesDeFalla),
                 Estado = informe.Estado
             };
         }
