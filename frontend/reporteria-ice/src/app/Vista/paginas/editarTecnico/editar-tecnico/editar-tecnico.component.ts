ec66c651f00e60e9db71447d44a77cd9f3d65c40
D240021 <danielbarrantesq@gmail.com>
2024-10-24 16:08:27 -0600

    Se agregan validaciones de formularios en las paginas de operarios en el menu de administrador

--- frontend/reporteria-ice/src/app/Vista/paginas/editarTecnico/editar-tecnico/editar-tecnico.component.ts
+++ frontend/reporteria-ice/src/app/Vista/paginas/editarTecnico/editar-tecnico/editar-tecnico.component.ts
@@ -1,27 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { BuscadorComponent } from '../../../componentes/buscador/buscador/buscador.component';
 import { RouterLink } from '@angular/router';
 import { Tecnico } from '../../../../Modelo/tecnico';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormulariosService } from '../../../../Util/Formularios/formularios.service';
 
 @Component({
   selector: 'editar-tecnico',
   standalone: true,
-  imports: [MatInputModule, MatButtonModule, BuscadorComponent, RouterLink],
+  imports: [MatInputModule, MatButtonModule, BuscadorComponent, RouterLink, ReactiveFormsModule],
   templateUrl: './editar-tecnico.component.html',
   styleUrl: './editar-tecnico.component.css'
 })
 export class EditarTecnicoComponent {
 
-  formularioVisible!: boolean;
+  public formularioVisible!: boolean;
 
-  datosQuemados = [
+  public datosQuemados = [
     { nombre: 'D242001'},
     { nombre: 'STF46' },
   ];
 
-  tecnicosQuemados: Tecnico[] = [
+  public tecnicosQuemados: Tecnico[] = [
     {
       nombreUsuario : 'D242001',
       contrasenia : '12245sff',
@@ -35,6 +37,15 @@ export class EditarTecnicoComponent {
       unidadRegional : 'Orosi'
     },
   ]
+
+  private formBuilder = inject(FormBuilder);
+  public accionesFormulario = inject(FormulariosService);
+
+  public contenedorFormulario = this.formBuilder.group({
+    nombreUsuario: ['', {validators: [Validators.required]}],
+    contrasenia: ['', {validators: [Validators.required]}],
+    unidadRegional: ['', {validators: [Validators.required]}],
+  });
 
 
   procesarBusqueda(nombreUsuarioBuscado: string): void {
