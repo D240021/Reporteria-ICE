85ca9930fbf95098dc0d7565a59d1f08d79317f7
Josué Araya Marín <104031367+Josue19-08@users.noreply.github.com>
2024-10-24 11:18:05 -0600

    Alvaro backend (#16)
    * Modificaciones para Registro de Reporte, Informe y Datos de Informe __ (Incompleto aun)
    
    * Coreccion de Error de Sintaxis en ReporteController
    
    * Correcciones para la migracion: Reporte e Informe con menos instancias completas
    
    * Reutilizacion de los Mapper de los datos de Informe para InformeDTOMapper
    
    * Correccion de Errores de Compilacion
    
    * Se Agregan Las Instancias de Datos de Informe E Informe a la BD
    
    * Migraciones de rama AlvaroBackend
    
    ---------
    
    Co-authored-by: Alvaro <alvarosiles499@gmail.com>

---
+++ Backend/ICE.Capa_Datos/Migrations/20241024025340_MigracionParaAgregarReportesInformesDatos.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ICE.Capa_Datos.Migrations
+{
+    /// <inheritdoc />
+    public partial class MigracionParaAgregarReportesInformesDatos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<TimeSpan>(
+                name: "Hora",
+                table: "DatosGenerales",
+                type: "time",
+                nullable: true,
+                oldClrType: typeof(TimeSpan),
+                oldType: "time");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Fecha",
+                table: "DatosGenerales",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<TimeSpan>(
+                name: "Hora",
+                table: "DatosGenerales",
+                type: "time",
+                nullable: false,
+                defaultValue: new TimeSpan(0, 0, 0, 0, 0),
+                oldClrType: typeof(TimeSpan),
+                oldType: "time",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Fecha",
+                table: "DatosGenerales",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+        }
+    }
+}
