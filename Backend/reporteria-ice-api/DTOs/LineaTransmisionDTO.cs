a093cad2b0da31c5d83346a044f18bde08a1ec94
Daniel Barrantes <danielbarrantesq@gmail.com>
2024-10-26 11:30:29 -0600

    Add .gitignore and remove unnecessary files

--- Backend/reporteria-ice-api/DTOs/LineaTransmisionDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace reporteria_ice_api.DTOs
-{
-    public class LineaTransmisionDTO
-    {
-        public int? Id { get; set; } 
-        public string NombreUbicacion { get; set; }
-        public int Identificador { get; set; }
-    }
-}
--- Backend/reporteria-ice-api/DTOs/UnidadRegionalDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace reporteria_ice_api.DTOs
-{
-    public class UnidadRegionalDTO
-    {
-        public int? Id { get; set; }
-        public string NombreUbicacion { get; set; }
-        public int Identificador { get; set; }
-    }
-}
